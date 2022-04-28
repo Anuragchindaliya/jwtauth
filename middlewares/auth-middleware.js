64364766fc5235d666aab10bb89660df9e34c63e
Anuragchindaliya <ak1148383@gmail.com>
2022-04-28 13:40:36 +0530

    add:user authenctication middleware

---
+++ middlewares/auth-middleware.js
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+import UserModel from "../models/User.js";
+var checkUserAuth = async(req, res, next) => {
+
+    const { cookies } = req;
+    if ("token" in cookies) {
+        console.log("session id exists", cookies);
+        try {
+            const { signID } = jwt.verify(cookies.token, process.env.JWT_SECRET_KEY);
+            res.user = await UserModel.findById(signID).select("-password");
+            next()
+        } catch (error) {
+            console.log(error);
+            res.status(401).json({ status: "failed", message: "Unauthorize user" });
+        }
+    } else {
+        console.log("cookies not exists");
+        res.status(403).json({ status: "failed", message: "Not Authenticated" })
+    }
+
+}
+export default checkUserAuth;
