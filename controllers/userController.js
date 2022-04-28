b1d4bf4e02ad83b28f198b6c1464b0bf61a2cb64
Anuragchindaliya <ak1148383@gmail.com>
2022-04-29 00:45:05 +0530

    remove:unnecessary fields

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,38 +3,35 @@ import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken";
 class UserController {
     static userRegistration = async (req, res,) => {
-        const { name, email, password, password_confirmation } = req.body;
+        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+        const { name, email, password } = req.body;
 
-        if (name && email && password && password_confirmation) {
-            if (password === password_confirmation) {
-                const user = await UserModel.findOne({ email: email });
+        if (name && email && password) {
 
-                if (user === null) {
-                    try {
-                        const salt = await bcrypt.genSalt(10)
-                        const hashPassword = await bcrypt.hash(password, salt);
-                        const doc = new UserModel({
-                            name: name,
-                            email: email,
-                            password: hashPassword
-                        })
-                        await doc.save();
-                        const saved_user = await UserModel.findOne({ email: email })
-                        // genrate jwt token
-                        const token = jwt.sign({ signID: saved_user._id }, process.env.JWT_SECRET_KEY, { expiresIn: "5d" });
-                        res.cookie("token", token);
-                        res.status(201).json({ status: "success", message: "Registration success" })
-                    } catch (error) {
-                        console.log(error);
-                        res.send({ "status": "failes", "message": "unable to register" })
-                    }
+            const user = await UserModel.findOne({ email: email });
+
+            if (user === null) {
+                try {
+                    const salt = await bcrypt.genSalt(10)
+                    const hashPassword = await bcrypt.hash(password, salt);
+                    const doc = new UserModel({
+                        name: name,
+                        email: email,
+                        password: hashPassword
+                    })
+                    await doc.save();
+                    const saved_user = await UserModel.findOne({ email: email })
+                    // genrate jwt token
+                    const token = jwt.sign({ signID: saved_user._id }, process.env.JWT_SECRET_KEY, { expiresIn: "5d" });
+                    res.cookie("token", token);
+                    res.status(201).json({ status: "success", message: "Registration success" })
+                } catch (error) {
+                    console.log(error);
+                    res.send({ "status": "failes", "message": "unable to register" })
                 }
-                else {
-                    res.status(209).send({ "status": "failed", "message": "email is already exists" });
-                }
-
-            } else {
-                res.send({ "status": "failed", "message": "password and confirmation password doestn't match" })
+            }
+            else {
+                res.status(209).send({ "status": "failed", "message": "email is already exists" });
             }
         } else {
             res.send({ "status": "failed", "message": "All fields are required" })
