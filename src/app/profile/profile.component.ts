8011ac59f7eed6b334bcbcb151059252cdb196f8
Anuragchindaliya <ak1148383@gmail.com>
2022-04-29 23:36:44 +0530

    add:logout feature - with logout api and delete cookie

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { HttpService } from '../services/http.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +11,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _auth: HttpService, private _cookie: CookieService, private router: Router) { }
 
   ngOnInit(): void {
   }
-
+  logout() {
+    this._auth.logoutUser().subscribe((res: any) => {
+      if (res.status === "success") {
+        this._cookie.delete("token")
+        this.router.navigate(["/login"]);
+      }
+    })
+  }
 }
