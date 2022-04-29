98fe115bd14a0b7b1ce18d0306e50ccedc5ca4c4
Anuragchindaliya <ak1148383@gmail.com>
2022-04-29 23:28:51 +0530

    Add:authgaurd to check user Auth for profile page

--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -1,16 +1,24 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
+import { HttpService } from './http.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  constructor(private _auth: HttpService) { }
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-
-    return true;
+    return this._auth.checkUserAuth().pipe(map((res: any) => {
+      if (res.status === "success") {
+        return true;
+      } else {
+        return false;
+      }
+    }))
   }
+
 
 }
