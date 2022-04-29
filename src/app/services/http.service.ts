67c1bb0989bb283a414c1f6e1aa5a871646031f9
Anuragchindaliya <ak1148383@gmail.com>
2022-04-29 23:27:09 +0530

    add: Check user Auth and logout

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,18 +1,30 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
-  private _registerUrl = "http://localhost:8000/api/user/register";
-  private _loginUrl = "http://localhost:8000/api/user/login";
+  baseurl = "http://localhost:8000";
+  private _registerUrl = `${this.baseurl}/api/user/register`;
+  private _loginUrl = `${this.baseurl}/api/user/login`;
+  private _checkAuthUrl = `${this.baseurl}/api/user/checkauth`;
 
-  constructor(private _http: HttpClient) { }
+  constructor(private _http: HttpClient, private _cookieService: CookieService) { }
   registerUser(user: any) {
     return this._http.post<any>(this._registerUrl, user)
   }
   loginUser(user: any) {
     return this._http.post<any>(this._loginUrl, user)
+  }
+
+  checkUserAuth() {
+    const token = this._cookieService.get("token");
+    const tokenHeader = new HttpHeaders({ Authorization: `Bearer ${token}` })
+    return this._http.get(this._checkAuthUrl, { headers: tokenHeader })
+  }
+  logoutUser() {
+    return this._http.get("http://localhost:8000/api/user/logout")
   }
 }
